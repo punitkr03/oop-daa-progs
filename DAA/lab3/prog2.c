b872f5c2b0cc1ab719cfb6bcd98391d5c30e4c4a
Punit Kumar <tagpunitkumar@gmail.com>
2023-11-03 18:56:19 +0530

    lab3

---
+++ DAA/lab3/prog2.c
@@ -0,0 +1,59 @@
+// Program to implement quick sort.
+#include <stdio.h>
+#include <stdlib.h>
+
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int s, int e)
+{
+    int pivot = arr[e];
+    int i = s - 1;
+    for (int j = s; j < e; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[e]);
+    return i + 1;
+}
+
+void quicksort(int arr[], int s, int e)
+{
+    if (s < e)
+    {
+        int p = partition(arr, s, e);
+        quicksort(arr, s, p - 1);
+        quicksort(arr, p + 1, e);
+    }
+}
+
+int main()
+{
+    int n = 8;
+    int arr[] = {7, 6, 3, 2, 1, 8, 5, 4};
+
+    printf("Before sorting:\n");
+    display(arr, n);
+    quicksort(arr, 0, n - 1);
+    printf("After sorting:\n");
+    display(arr, n);
+
+    return 0;
+}
---
+++ DAA/lab3/prog3.c
@@ -0,0 +1,63 @@
+// Program to implement heap sort.
+#include <stdio.h>
+#include <stdlib.h>
+
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i)
+    {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapsort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main()
+{
+    int n = 8;
+    int arr[] = {7, 6, 3, 2, 1, 8, 5, 4};
+
+    printf("Before sorting:\n");
+    display(arr, n);
+    heapsort(arr, n);
+    printf("After sorting:\n");
+    display(arr, n);
+
+    return 0;
+}
